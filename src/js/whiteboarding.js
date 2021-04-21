2b3c12e1f66c97de8b4c1a76d49f912c072cb8d5
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-21 11:29:22 -0700

    update html
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

---
+++ src/js/whiteboarding.js
@@ -0,0 +1,4 @@
+//user input birthdate
+//search api for user birthdate
+//return asteroid information based on user birthdate
+//display asteroid information in DOM
--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import './css/styles.css';
 
 function getElements(response) {
   if (response.main) {
-    $("#show-count").text(`<p>There were ${response.element_count} asteroids near Earth on your birthday. </p>`)
-    $("#show-dist").text(`<p>The object was ${response.near_earth_objects.${ $date }.} `)}
+    $("#show-count").text(`<p>There were ${response.element_count} asteroids near Earth on your birthday. </p>`);
+    // $("#show-dist").text(`<p>The object was ${response.near_earth_objects.${date}[0].close_approach_data[0].miss_distance.miles} miles away.</p>`);
   }
 }
+
+
+
+$(document).ready(function () {
